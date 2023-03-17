b9df12c2a70ca37939d9313dee0adbf8768847f0
anushreeSonak <sonakanushree@gmail.com>
2023-03-17 12:44:17 +0530

    Adding Multiple Interface Program 17 March

---
+++ First/src/tutorials/MultipleInterface.java
@@ -0,0 +1,30 @@
+package tutorials;
+
+
+interface FirstInterface {
+	  public void myMethod(); // interface method
+	}
+
+	interface SecondInterface {
+	  public void myOtherMethod(); // interface method
+	}
+
+	class DemoClass implements FirstInterface, SecondInterface {
+	  public void myMethod() {
+	    System.out.println("Some text..");
+	  }
+	  public void myOtherMethod() {
+	    System.out.println("Some other text...");
+	  }
+	}
+
+public class MultipleInterface {
+
+	public static void main(String[] args) 
+	{
+		DemoClass myObj = new DemoClass();
+	    myObj.myMethod();
+	    myObj.myOtherMethod();
+	}
+
+}
