afa98bcadca90f3645068a77b17a433859ad46fa
anushreeSonak <sonakanushree@gmail.com>
2023-04-03 17:54:20 +0530

    Added updated code of MultipleArray

--- First/src/tutorials/MultipleArray.java
+++ First/src/tutorials/MultipleArray.java
@@ -1,29 +1,27 @@
 package tutorials;
 
 public class MultipleArray {
-
     public static void main(String[] args) {
         public static void printArrays (String[]arrOne,int[] arrTwo)
         {
-			System.out.println("Printing string array:");
-		}
-            for (String s : arrOne) {
-                System.out.println(s);
-            }
-            System.out.println("Printing integer array:");
-            for (int i : arrTwo) {
-                System.out.println(i);
-            }
+            System.out.println("Printing string Array:");
         }
-
-        public static void main (String[]args)
-        {
-            String[] strArr = {"apple", "banana", "orange"};
-            int[] intArr = {1, 2, 3, 4};
-            // Calling the new method and passing in the arrays
-            printArrays(strArr, intArr);
+        for (String s : arrOne) {
+            System.out.println(s);
+        }
+        System.out.println("Printing integer Array:");
+        for (int i : arrTwo) {
+            System.out.println(i);
         }
     }
+
+    public static void main(String[] args) {
+        String[] strArr = {"apple", "banana", "orange"};
+        int[] intArr = {1, 2, 3, 4};
+        // Calling the new method and passing in the arrays
+        printArrays(strArr, intArr);
+    }
+}
 
 
 
