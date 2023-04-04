60b61021340eb073c21f37f5f1808dc888a2b0d8
anushreeSonak <sonakanushree@gmail.com>
2023-04-04 15:29:46 +0530

    added updated code of stack program

--- First/src/tutorials/StackOperations.java
+++ First/src/tutorials/StackOperations.java
@@ -1,26 +1,20 @@
-package tutorials;
+package firstPackage.First.src.tutorials;
 
 import java.util.Stack;
 
-public class  {
+public class StackOperations {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+        //Pushing elements to the stack
+        stack.push("India");
+        stack.push("Canada");
+        stack.push("New York");
+        stack.push("China");
+        System.out.println("Stack: " + stack);
+        //to remove/pop the element form the stack
+        String poppedElement = stack.pop();
+        System.out.println("Popped element is: " + poppedElement);
+        System.out.println("Updated stack is : " + stack);
 
-	public static void main(String[] args)
-	{
-		Stack<String> stack = new Stack<>();
-		
-		//Pushing elements to the stack
-		
-		stack.push("India");
-		stack.push("Canada");
-		stack.push("New York");
-		stack.push("China");
-		
-		System.out.println("Stack: "+ stack);
-		//to remove/pop the element form the stack 
-		String poppedElement= stack.pop();
-		System.out.println("Popped element is: "+poppedElement );
-		System.out.println("Updated stack is : "+ stack);
-				
-	}
-
+    }
 }
