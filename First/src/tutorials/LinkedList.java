8b8c15a761ef15a29bb8415e9c587a5dfd18a78d
anushreeSonak <sonakanushree@gmail.com>
2023-04-06 13:20:37 +0530

    Added Updated code of ArrayList

---
+++ First/src/tutorials/LinkedList.java
@@ -0,0 +1,18 @@
+package tutorials;
+
+public class LinkedList {
+
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<String>();
+        list.add("A");
+        list.add("B");
+        list.addLast("c");
+        list.addFirst("D");
+        list.add(2, "E");
+        System.out.println(list);
+        list.remove("A");
+        list.removeFirst();
+        list.removeLast();
+        System.out.println(list);
+    }
+}
