357aecd83191fcc467c48646b57e7b24246257a6
anushreeSonak <sonakanushree@gmail.com>
2023-04-06 15:26:35 +0530

    Added Updated code of medium complexity problem-3-- Swapper

--- First/src/MediumDifficultyAssignmnets/Swapper.java
+++ First/src/MediumDifficultyAssignmnets/Swapper.java
@@ -1,42 +1,29 @@
+package firstPackage.First.src.MediumDifficultyAssignmnets;
 
 import java.util.Arrays;
 
 public class Swapper {
-    public static void main(String[] args) {
-        int[] arr1 = {1, 2,3,4,5};
-        int[] arr2 = {1, 2, 3};
-
-
-        System.out.println(Arrays.toString(swapEnds(arr1)));
-        System.out.println(Arrays.toString(swapEnds(arr2)));
+    public void CheckPrimeNum(int[] intArray) {
+        int temp = 0;
+        int lastNum = intArray[intArray.length - 1];
+        if (lastNum % 2 == 0) {
+            System.out.println("The last number of array is not prime number: " + lastNum);
+        } else {
+            temp = intArray[0];
+            intArray[0] = intArray[intArray.length - 1];
+            intArray[intArray.length - 1] = temp;
+            System.out.println("The last number of array is  prime number: " + lastNum);
+            System.out.println("The swap array is: " + Arrays.toString(intArray));
+        }
     }
 
-    public static int[] swapEnds(int[] arr) {
-        int lastElement = arr[arr.length - 1];
-
-        // Check if the last element is prime
-        boolean isPrime = true;
-        if (lastElement < 2) {
-            isPrime = false;
-        } else {
-            for (int i = 2; i <= Math.sqrt(lastElement); i++) {
-                if (lastElement % i == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-        }
-
-        // Swap the first and last elements
-        int temp = arr[0];
-        arr[0] = arr[arr.length - 1];
-        arr[arr.length - 1] = temp;
-
-        // Return the modified array
-        if (isPrime) {
-            return arr;
-        } else {
-            return new int[arr.length];
-        }
+    public static void main(String[] args) {
+        Swapper primeNumber = new Swapper();
+        int[] Array = new int[]{1, 2, 3, 7};
+        int[] ArrayOne = new int[]{1, 2, 3, 4, 12};
+        int[] ArrayTwo = new int[]{1, 6, 7, 9, 8};
+        primeNumber.CheckPrimeNum(Array);
+        primeNumber.CheckPrimeNum(ArrayOne);
+        primeNumber.CheckPrimeNum(ArrayTwo);
     }
 }
