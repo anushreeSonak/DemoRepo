cb5ec0138b1417c78ae13dcb49a039dc9c0f16b7
anushreeSonak <sonakanushree@gmail.com>
2023-04-06 15:57:45 +0530

    Added Medium Complexity Problem 1 program--AnnualSalary

--- First/src/MediumDifficultyAssignmnets/AnnualSalary.java
+++ First/src/MediumDifficultyAssignmnets/AnnualSalary.java
@@ -1,42 +1,45 @@
 package firstPackage.First.src.MediumDifficultyAssignmnets;
 
 public class AnnualSalary {
-    private double totalTax;
-    private int IncomeCases;
+    private static float tax;
+    private static float taxableAmount;
 
-    public void findIncomeTax(int taxableIncome) {
-        IncomeCases = (taxableIncome <= 700000) ? 1 : (taxableIncome > 500000 && taxableIncome <= 1000000) ? 2 : (taxableIncome <= 1500000) ? 3 : 4;
-
-        switch (IncomeCases) {
+    public float displayIncomeTax(float income) {
+        int Incomeslab = (income < 500000) ? 1 : (500000 <= income && income <= 700000)
+                ? 2 : (700000 < income && income <= 1000000) ? 3 : (income <= 1500000) ? 4 : 5;
+        switch (Incomeslab) {
             case 1:
-                totalTax = 0;
+                System.out.println("you are tax free ");
                 break;
             case 2:
-                totalTax = (0.15 * (taxableIncome - 700000)) + (0.1 * 200000);
+                taxableAmount = income - 500000;
+                tax = (0.10f) * taxableAmount;
+                System.out.println("you are in 10 %");
                 break;
             case 3:
-                totalTax = (0.2 * (taxableIncome - 1000000)) + (0.15 * 300000) + (0.1 * 200000);
+                taxableAmount = income - 700000;
+                tax = (0.15f) * taxableAmount + (0.1F * 200000);
+                System.out.println("you are in 15 %");
                 break;
             case 4:
-                totalTax = (0.3 * (taxableIncome - 1500000)) + (0.2 * 500000) + (0.15 * 300000) + (0.1 * 200000);
+                taxableAmount = income - 1000000;
+                tax = (0.20f) * taxableAmount + (0.15F * 300000) + (0.1F * 200000);
+                System.out.println("you are in 20 %");
+                break;
+            case 5:
+                taxableAmount = income - 1000000;
+                tax = (0.30f) * taxableAmount + (0.2F * 500000) + (0.15F * 300000) + (0.1F * 200000);
+                System.out.println("you are in 30 %");
                 break;
             default:
-                System.out.println("Enter income");
+                System.out.println("Enter valid input");
         }
-        System.out.println("Income tax amount is :" + totalTax);
+        System.out.println("you are taxAmount is" + tax);
+        return tax;
     }
 
     public static void main(String[] args) {
-        AnnualSalary incomeTaxCalculation = new AnnualSalary();
-        System.out.println("\nTotal Taxable Income : 700000");
-        incomeTaxCalculation.findIncomeTax(700000);
-        System.out.println("\nTotal Taxable Income : 800000");
-        incomeTaxCalculation.findIncomeTax(800000);
-        System.out.println("\nTotal Taxable Income : 1400000");
-        incomeTaxCalculation.findIncomeTax(1400000);
-        System.out.println("\nTotal Taxable Income : 1600000");
-        incomeTaxCalculation.findIncomeTax(1600000);
+        AnnualSalary taxCalculator = new AnnualSalary();
+        taxCalculator.displayIncomeTax(900000);
     }
 }
-
-
