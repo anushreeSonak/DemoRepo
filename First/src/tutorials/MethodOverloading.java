3517bf248ae55f53ec1bc86653a5d0b5e64d88e4
anushreeSonak <sonakanushree@gmail.com>
2023-04-06 13:27:26 +0530

    Added updated code of MethodOverloading

--- First/src/tutorials/MethodOverloading.java
+++ First/src/tutorials/MethodOverloading.java
@@ -1,37 +1,23 @@
 package tutorials;
 
-public class MethodOverloading
-{
+public class MethodOverloading {
 
-	public int Multiply(int X, int Y)
-	{
-		return(X*Y);
-	}
+    public int Multiply(int varOne, int varTwo) {
+        return (varOne * varTwo);
+    }
 
+    public int Multiply(int varOne, int varTwo, int varThree) {
+        return (varOne * varTwo * varThree);
+    }
 
-	public int Multiply(int X, int Y, int Z)
-	{
-		return(X*Y*Z);
-	}
+    public double Multiply(double varOne, double varTwo) {
+        return (varOne * varTwo);
+    }
 
-
-	public double Multiply(double X, double Y)
-	{
-		return(X*Y);
-	}
-
-
-	public static void main(String args[])
-	{
-		MethodOverloading MO= new MethodOverloading();
-		System.out.println(MO.Multiply(10, 50));
-		System.out.println(MO.Multiply(10, 50,20));
-		System.out.println(MO.Multiply(2.5, 5.0));
-
-
-
-	}
+    public static void main(String args[]) {
+        MethodOverloading MO = new MethodOverloading();
+        System.out.println(MO.Multiply(20, 50));
+        System.out.println(MO.Multiply(10, 50, 20));
+        System.out.println(MO.Multiply(2.5, 5.0));
+    }
 }
-
-
-
