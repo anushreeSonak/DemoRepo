be84ec43f1c6a702130ca015b82ae6a8311a1171
anushreeSonak <sonakanushree@gmail.com>
2023-04-07 13:37:12 +0530

    added Updated Program of  Medium Complexity Problem - 3

--- First/src/MediumDifficultyAssignmnets/Swapper.java
+++ First/src/MediumDifficultyAssignmnets/Swapper.java
@@ -3,24 +3,24 @@ package firstPackage.First.src.MediumDifficultyAssignmnets;
 import java.util.Arrays;
 
 public class Swapper {
-    private int temp;
-    private int lastNumber;
-
-    public void endSwap(int arrayOne[]) {
-
-        int lastElement = arrayOne[arrayOne.length - 1];
-        if (lastElement % 2 != 0) {
-            int temporary = 0;
-            temporary = arrayOne[0];
-            arrayOne[0] = arrayOne[arrayOne.length - 1];
-            arrayOne[arrayOne.length - 1] = temporary;
-        }
-        System.out.println(Arrays.toString(arrayOne));
+    public static void main(String[] args) {
+        Swapper swapObject = new Swapper();
+        int[] inputArray = {1, 2, 3, 7};
+        int[] inputArraySecond = {1, 2, 3};
+        int[] inputArrayThird = {1, 6, 7, 9, 8, 12};
+        swapObject.swapPrimeNumber(inputArray);
+        swapObject.swapPrimeNumber(inputArraySecond);
+        swapObject.swapPrimeNumber(inputArrayThird);
     }
 
-    public static void main(String[] args) {
-        Swapper swapArray = new Swapper();
-        int[] arrayOne = new int[]{1, 2, 3, 7};
-        swapArray.endSwap(arrayOne);
+    public void swapPrimeNumber(int[] inputArray) {
+        int lastElement = inputArray[inputArray.length - 1];
+        if (lastElement % 2 != 0) {
+            int temporary = 0;
+            temporary = inputArray[0];
+            inputArray[0] = inputArray[inputArray.length - 1];
+            inputArray[inputArray.length - 1] = temporary;
+        }
+        System.out.println(Arrays.toString(inputArray));
     }
 }
