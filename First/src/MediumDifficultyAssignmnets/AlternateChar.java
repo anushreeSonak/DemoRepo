368a44e5f2d2581c83f11d0d8878e63dc953f7e1
anushreeSonak <sonakanushree@gmail.com>
2023-04-10 13:30:12 +0530

    Added Code of Medium Complexity Problem-4

--- First/src/MediumDifficultyAssignmnets/AlternateChar.java
+++ First/src/MediumDifficultyAssignmnets/AlternateChar.java
@@ -1,2 +1,19 @@
-package firstPackage.First.src.MediumDifficultyAssignmnets;public class AlternateChar {
+package firstPackage.First.src.MediumDifficultyAssignmnets;
+
+public class AlternateChar {
+    public static void main(String args[]) {
+        String result = mergeStringAlternatively("abc", "xyz");
+        System.out.println(result);
+    }
+
+    public static String mergeStringAlternatively(String firstString, String secondString) {
+        String mergeString = "";
+        for (int i = 0; i < firstString.length() || i < secondString.length(); i++) {
+            if (i < firstString.length())
+                mergeString += firstString.charAt(i);
+            if (i < secondString.length())
+                mergeString += secondString.charAt(i);
+        }
+        return mergeString;
+    }
 }
