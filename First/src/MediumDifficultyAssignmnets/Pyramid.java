1dd82cbd60113c948e1fbcb64b1286c63b3c9a8b
anushreeSonak <sonakanushree@gmail.com>
2023-04-11 13:27:50 +0530

    Added Updated code of Medium Comp problem-2

--- First/src/MediumDifficultyAssignmnets/Pyramid.java
+++ First/src/MediumDifficultyAssignmnets/Pyramid.java
@@ -1,21 +1,29 @@
-package test;
+package firstPackage.First.src.MediumDifficultyAssignmnets;
 
 public class Pyramid {
     public static void main(String args[]) {
         patternMethod(4);
     }
+
     public static void patternMethod(int row) {
-        for (int i = 1; i <= row; i++) {
-            for (int j = 1; j <= row - i; j++) {
+        for (int VarOne = 1; VarOne <= row; VarOne++) {
+            for (int VarTwo = 1; VarTwo <= row - VarOne; VarTwo++) {
                 System.out.print(" ");
             }
-            for (int k = i; k >= 1; k--) {
-                System.out.print(k);
+            for (int VarThree = VarOne; VarThree >= 1; VarThree--) {
+                System.out.print(VarThree);
             }
-            for (int l = 2; l <= i; l++) {
-                System.out.print(l);
+            for (int VarFour = 2; VarFour <= VarOne; VarFour++) {
+                System.out.print(VarFour);
             }
             System.out.println();
         }
     }
 }
+//OUTPUT:-
+// "C:\Program Files\Java\jdk-17\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=61761:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -classpath C:\Users\anushrees\Desktop\Demo\DemoRepo\out\production\DemoRepo firstPackage.First.src.MediumDifficultyAssignmnets.Pyramid
+//   1
+//  212
+// 32123
+//4321234
+//Process finished with exit code 0
