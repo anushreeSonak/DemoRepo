d12998aa565ae599adc08c569b52705f72f4f795
anushreeSonak <sonakanushree@gmail.com>
2023-04-11 14:43:57 +0530

    Added updated code of ArrayList Program

--- First/src/tutorials/LanguageList.java
+++ First/src/tutorials/LanguageList.java
@@ -1,18 +1,19 @@
 package firstPackage.First.src.tutorials;
 
-public class ArrayList {
-    public static void main(String[] args){
+import java.util.ArrayList;
 
+public class LanguageList {
+    public static void main(String[] args) {
         // create ArrayList
         ArrayList<String> languages = new ArrayList<>();
-
-        // Add elements to ArrayList
+        // add() method without the index parameter
         languages.add("Java");
+        languages.add("C");
         languages.add("Python");
-        languages.add("Swift");
         System.out.println("ArrayList: " + languages);
-
-        ArrayList arrayListOne = new ArrayList();
-        ar
     }
 }
+//OUTPUT:-
+// "C:\Program Files\Java\jdk-17\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=61417:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -classpath C:\Users\anushrees\Desktop\Demo\DemoRepo\out\production\DemoRepo firstPackage.First.src.tutorials.LanguageList
+//ArrayList: [Java, C, Python]
+//Process finished with exit code 0
