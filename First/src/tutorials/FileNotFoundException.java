e1ccc61f2c61d2af34772572bfd4b7694d7588a7
anushreeSonak <sonakanushree@gmail.com>
2023-04-11 14:39:10 +0530

    Added updated code of FileNotFoundException Program

--- First/src/tutorials/FileNotFoundException.java
+++ First/src/tutorials/FileNotFoundException.java
@@ -1,15 +1,23 @@
-package tutorials;
-import java.io.File;
-import java.io.FileReader;
+package firstPackage.First.src.tutorials;
+
 import java.io.IOException;
+import java.io.FileInputStream;
 
-public class FileNotFoundException
-{
-
-	public static void main(String args[]) throws IOException 
-	{		
-		File file = new File("E://file.txt");
-		FileReader fr = new FileReader(file); 
-	}
+public class FileNotFoundException {
+    public static void main(String[] args) {
+        try {
+            FileInputStream fileInputStream = new FileInputStream("C:\\Users\\anushrees\\Desktop\\Test.txt");
+            int data;
+            while ((data = fileInputStream.read()) != -1) {
+                System.out.print((char) data);
+            }
+            fileInputStream.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
 }
-
+//OUTPUT:-
+// "C:\Program Files\Java\jdk-17\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=51346:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -classpath C:\Users\anushrees\Desktop\Demo\DemoRepo\out\production\DemoRepo firstPackage.First.src.tutorials.FileNotFoundException
+//java.io.FileNotFoundException: C:\Users\anushrees\Desktop\Test.txt (The system cannot find the file specified)
+//Process finished with exit code 0
