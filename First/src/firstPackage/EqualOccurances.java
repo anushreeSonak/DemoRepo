95399132ec342f0f0760047fd538d5c566797ba7
anushreeSonak <sonakanushree@gmail.com>
2023-04-11 13:33:29 +0530

    Added Updated code of EqualOccurances

--- First/src/firstPackage/EqualOccurances.java
+++ First/src/firstPackage/EqualOccurances.java
@@ -1,35 +1,39 @@
 package firstPackage.First.src.firstPackage;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class EqualOccurances {
-    public static boolean equalNumberOccurances(String input) {
-        boolean output = true;
-        int firstCount = 1;
-        for (int count = 0; count < input.length(); count++) {
-            char stringchar = input.charAt(count);
-            int secondCount = 1;
-            for (int i = 0; i < input.length(); i++) {
-                if (count == i)
-                    continue;
-                if (stringchar == input.charAt(i)) {
-                    if (count == 0) {
-                        firstCount++;
-                    }
-                    secondCount++;
-                }
-            }
-            if (firstCount != secondCount) {
-                output = false;
-                return output;
+    public static boolean NumberOfOccurances(String input) {
+        Map<Character, Integer> countOccurances = new HashMap<>();
+        char[] charOne = input.toCharArray();
+        for (char ch : charOne) {
+            if (countOccurances.get(ch) != null) {
+                countOccurances.put(ch, countOccurances.get(ch) + 1);
+            } else {
+                countOccurances.put(ch, 1);
             }
         }
-        return output;
+        Set<Integer> setOne = new HashSet<>();
+        setOne.addAll(countOccurances.values());
+        if (setOne.size() == 1) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static void main(String[] args) {
-        boolean output = equalNumberOccurances("abacbc");
-        if (output == true)
-            System.out.println("All Characters Have Equal Number of Occurrences");
+        boolean Result = NumberOfOccurances("aaabbbccc");
+        if (Result)
+            System.out.println("All Characters Have equal Number of Occurrences");
         else
-            System.out.println("All Characters Have Not Equal Number of Occurrences");
+            System.out.println("All Characters Have NOT equal Number of Occurrences");
     }
 }
+//OUTPUT:-
+// "C:\Program Files\Java\jdk-17\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=51168:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -classpath C:\Users\anushrees\Desktop\Demo\DemoRepo\out\production\DemoRepo firstPackage.First.src.firstPackage.EqualOccurances
+//All Characters Have equal Number of Occurrences
+//Process finished with exit code 0
