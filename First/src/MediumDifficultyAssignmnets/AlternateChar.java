a4c4e69a54168f37febf975b9e4240f07167d89f
anushreeSonak <sonakanushree@gmail.com>
2023-04-11 13:21:51 +0530

    Added updated code of Medium Compl Problem-4

--- First/src/MediumDifficultyAssignmnets/AlternateChar.java
+++ First/src/MediumDifficultyAssignmnets/AlternateChar.java
@@ -8,12 +8,16 @@ public class AlternateChar {
 
     public static String mergeStringAlternatively(String firstString, String secondString) {
         String mergeString = "";
-        for (int i = 0; i < firstString.length() || i < secondString.length(); i++) {
-            if (i < firstString.length())
-                mergeString += firstString.charAt(i);
-            if (i < secondString.length())
-                mergeString += secondString.charAt(i);
+        for (int VarOne = 0; VarOne < firstString.length() || VarOne < secondString.length(); VarOne++) {
+            if (VarOne < firstString.length())
+                mergeString += firstString.charAt(VarOne);
+            if (VarOne < secondString.length())
+                mergeString += secondString.charAt(VarOne);
         }
         return mergeString;
     }
 }
+//OUTPUT:-
+// "C:\Program Files\Java\jdk-17\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=62412:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -classpath C:\Users\anushrees\Desktop\Demo\DemoRepo\out\production\DemoRepo firstPackage.First.src.MediumDifficultyAssignmnets.AlternateChar
+//axbycz
+//Process finished with exit code 0
