fd4d71e9136569743ed2451427ed3eaceb232df1
anushreeSonak <sonakanushree@gmail.com>
2023-04-12 14:40:00 +0530

    Added updated code of MethodOverloading Program

--- First/src/tutorials/MethodOverloading.java
+++ First/src/tutorials/MethodOverloading.java
@@ -1,4 +1,4 @@
-package tutorials;
+package firstPackage.First.src.tutorials;
 
 public class MethodOverloading {
 
@@ -21,3 +21,9 @@ public class MethodOverloading {
         System.out.println(MO.Multiply(2.5, 5.0));
     }
 }
+//OUTPUT:-
+// "C:\Program Files\Java\jdk-17\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=61512:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -classpath C:\Users\anushrees\Desktop\Demo\DemoRepo\out\production\DemoRepo tutorials.MethodOverloading
+//1000
+//10000
+//12.5
+//Process finished with exit code 0
