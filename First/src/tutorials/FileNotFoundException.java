0c9fa10b6f2157f6df231299df307cc7ecd6fa53
anushreeSonak <sonakanushree@gmail.com>
2023-04-12 14:37:52 +0530

    Added updated code of FileNotFoundException Program

--- First/src/tutorials/FileNotFoundException.java
+++ First/src/tutorials/FileNotFoundException.java
@@ -17,6 +17,7 @@ public class FileNotFoundException {
         }
     }
 }
-//OUTPUT:-"C:\Program Files\Java\jdk-17\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=57005:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -classpath C:\Users\anushrees\Desktop\Demo\DemoRepo\out\production\DemoRepo firstPackage.First.src.tutorials.FileNotFoundException
+//OUTPUT:-
+// "C:\Program Files\Java\jdk-17\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=57005:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -classpath C:\Users\anushrees\Desktop\Demo\DemoRepo\out\production\DemoRepo firstPackage.First.src.tutorials.FileNotFoundException
 //java.io.FileNotFoundException: AnushreeFile.txt (The system cannot find the file specified)
 //Process finished with exit code 0
