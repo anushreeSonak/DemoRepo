8a396d8e0d7c9953560ec949af79ac332719c33b
anushreeSonak <sonakanushree@gmail.com>
2023-04-12 15:35:46 +0530

    Added updated code of StackOperations program

--- First/src/tutorials/StackOperations.java
+++ First/src/tutorials/StackOperations.java
@@ -3,21 +3,21 @@ package firstPackage.First.src.tutorials;
 import java.util.Stack;
 
 public class StackOperations {
-
-	public static void main(String[] args)
-	{
-		Stack<String> stack = new Stack<>();
-		stack.push("India");
-		stack.push("Canada");
-		stack.push("New York");
-		stack.push("China");
-		
-		System.out.println("Stack: "+ stack);
-		//to remove/pop the element form the stack 
-		String poppedElement= stack.pop();
-		System.out.println("Popped element is: "+poppedElement );
-		System.out.println("Updated stack is : "+ stack);
-				
-	}
-
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+        stack.push("India");
+        stack.push("Canada");
+        stack.push("New york");
+        stack.push("China");
+        System.out.println("Stack: " + stack);
+        String poppedElement = stack.pop();
+        System.out.println("Popped element is: " + poppedElement);
+        System.out.println("Updated stack is : " + stack);
+    }
 }
+//OUTPUT:-
+// "C:\Program Files\Java\jdk-17\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=49693:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -classpath C:\Users\anushrees\Desktop\Demo\DemoRepo\out\production\DemoRepo firstPackage.First.src.tutorials.StackOperations
+//Stack: [India, Canada, New york, China]
+//Popped element is: China
+//Updated stack is : [India, Canada, New york]
+//Process finished with exit code 0
