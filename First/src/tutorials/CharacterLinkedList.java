c9834650ffae5438a87f4901f74ab8d0439145ed
anushreeSonak <sonakanushree@gmail.com>
2023-04-12 15:27:07 +0530

    Added updated code of CharacterLinkedList program

--- First/src/tutorials/CharacterLinkedList.java
+++ First/src/tutorials/CharacterLinkedList.java
@@ -1,18 +1,20 @@
-package tutorials;
+package firstPackage.First.src.tutorials;
 
-public class LinkedList {
+import java.util.LinkedList;
+
+public class CharacterLinkedList {
 
     public static void main(String[] args) {
-        LinkedList<String> list = new LinkedList<String>();
-        list.add("A");
-        list.add("B");
-        list.addLast("c");
-        list.addFirst("D");
-        list.add(2, "E");
-        System.out.println(list);
-        list.remove("A");
-        list.removeFirst();
-        list.removeLast();
-        System.out.println(list);
+        LinkedList<String> ll = new LinkedList<String>();
+        ll.add("Ravi");
+        ll.add("Vijay");
+        ll.add("Anushree");
+        ll.add("Vikrant");
+        ll.add("Kanchan");
+        ll.add("Revati");
+        System.out.println("Initial list of elements: " + ll);
+        //Removing specific element from arraylist
+        ll.remove("Vijay");
+        System.out.println("After invoking remove(object) method: " + ll);
     }
 }
