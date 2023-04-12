182069de21ee4ccec38a3f46261574668bc697f8
anushreeSonak <sonakanushree@gmail.com>
2023-04-12 19:05:00 +0530

    Added Updated code of FindDestination program

--- First/src/firstPackage/FindDestination.java
+++ First/src/firstPackage/FindDestination.java
@@ -3,44 +3,33 @@ package firstPackage.First.src.firstPackage;
 import java.util.*;
 
 public class FindDestination {
-    public static String destCity(ArrayList<LinkedList> paths) {
-        String output = "";
-        List<String> startCity = new LinkedList<>();
-        List<String> endCity = new LinkedList<>();
-        for (LinkedList<String> path : paths) {
-            startCity.add(path.get(0));
-            endCity.add(path.get(1));
-        }
-        for (String inElement : endCity) {
-            if (!startCity.contains(inElement)) {
-                output = inElement;
-                break;
-            }
-        }
-        return output;
+    public static void main(String[] args) {
+        List<List<String>> paths = new ArrayList<>();
+        paths.add(Arrays.asList("London", "New York"));
+        paths.add(Arrays.asList("New York", "Lima"));
+        paths.add(Arrays.asList("Lima", "Sao Paulo"));
+        System.out.println(paths);
+        String destination = findDestination(paths);
+        System.out.println(destination);
     }
 
-    public static void main(String args[]) {
-        LinkedList<String> firstList = new LinkedList();
-        firstList.add("London");
-        firstList.add("New york");
-        LinkedList<String> secondList = new LinkedList();
-        secondList.add("New york");
-        secondList.add("Lima");
-        LinkedList<String> thirdList = new LinkedList();
-        thirdList.add("Lima");
-        thirdList.add("Sao paulo");
-        ArrayList<LinkedList> paths = new ArrayList<LinkedList>();
-        paths.add(firstList);
-        paths.add(secondList);
-        paths.add(thirdList);
-        System.out.println(paths);
-        String returnDest = destCity(paths);
-        System.out.println(returnDest);
+    public static String findDestination(List<List<String>> paths) {
+        Set<String> startCities = new HashSet<>();
+        Set<String> endCities = new HashSet<>();
+        for (List<String> path : paths) {
+            startCities.add(path.get(0));
+            endCities.add(path.get(1));
+        }
+        for (String endCity : endCities) {
+            if (!startCities.contains(endCity)) {
+                return endCity;
+            }
+        }
+        return null;
     }
 }
 //OUTPUT:-
-// "C:\Program Files\Java\jdk-17\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=63740:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -classpath C:\Users\anushrees\Desktop\Demo\DemoRepo\out\production\DemoRepo firstPackage.First.src.firstPackage.FindDestination
-//[[London, New york], [New york, Lima], [Lima, Sao paulo]]
-//Sao paulo
+// "C:\Program Files\Java\jdk-17\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=53022:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -classpath C:\Users\anushrees\Desktop\Demo\DemoRepo\out\production\DemoRepo firstPackage.First.src.firstPackage.FindDestination
+//[[London, New York], [New York, Lima], [Lima, Sao Paulo]]
+//Sao Paulo
 //Process finished with exit code 0
