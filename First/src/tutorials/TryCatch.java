b3d0a30435cef405c2106d74c04b0f2a3cbd7380
anushreeSonak <sonakanushree@gmail.com>
2023-04-12 14:51:06 +0530

    Added updated code of TryCatch program

--- First/src/tutorials/TryCatch.java
+++ First/src/tutorials/TryCatch.java
@@ -1,4 +1,4 @@
-package tutorials;
+package firstPackage.First.src.tutorials;
 
 public class TryCatch {
     public static void main(String[] args) {
@@ -10,3 +10,7 @@ public class TryCatch {
         }
     }
 }
+//OUTPUT:-
+// "C:\Program Files\Java\jdk-17\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=61481:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -classpath C:\Users\anushrees\Desktop\Demo\DemoRepo\out\production\DemoRepo tutorials.TryCatch
+//Something went wrong !!
+//Process finished with exit code 0
