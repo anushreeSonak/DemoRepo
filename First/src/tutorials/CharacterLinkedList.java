0ebd59c1db0589a0eaecbbd722d8b5e9d823ff2b
anushreeSonak <sonakanushree@gmail.com>
2023-04-17 18:01:06 +0530

    Added updated code of characterLinkedList program

--- First/src/tutorials/CharacterLinkedList.java
+++ First/src/tutorials/CharacterLinkedList.java
@@ -5,16 +5,21 @@ import java.util.LinkedList;
 public class CharacterLinkedList {
 
     public static void main(String[] args) {
-        LinkedList<String> ll = new LinkedList<String>();
-        ll.add("Ravi");
-        ll.add("Vijay");
-        ll.add("Anushree");
-        ll.add("Vikrant");
-        ll.add("Kanchan");
-        ll.add("Revati");
-        System.out.println("Initial list of elements: " + ll);
+        LinkedList<String> nameList = new LinkedList<String>();
+        nameList.add("Ravi");
+        nameList.add("Vijay");
+        nameList.add("Anushree");
+        nameList.add("Vikrant");
+        nameList.add("Kanchan");
+        nameList.add("Revati");
+        System.out.println("Initial list of elements: " + nameList);
         //Removing specific element from arraylist
-        ll.remove("Vijay");
-        System.out.println("After invoking remove(object) method: " + ll);
+        nameList.remove("Vijay");
+        System.out.println("After invoking remove(object) method: " + nameList);
     }
 }
+//OUTPUT:-
+// "C:\Program Files\Java\jdk-17\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=53900:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -classpath C:\Users\anushrees\Desktop\Demo\DemoRepo\out\production\DemoRepo firstPackage.First.src.tutorials.CharacterLinkedList
+//Initial list of elements: [Ravi, Vijay, Anushree, Vikrant, Kanchan, Revati]
+//After invoking remove(object) method: [Ravi, Anushree, Vikrant, Kanchan, Revati]
+//Process finished with exit code 0
