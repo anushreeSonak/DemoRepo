f7e0efffe6495d2021dc4b7d7dd645ca35facca1
Anushree Sonak <anushrees@synerzipune.local>
2023-05-22 12:50:43 +0530

    Added updated code of Coding exercise- 12

--- First/dailyExercises/ReverseLetters.java
+++ First/dailyExercises/ReverseLetters.java
@@ -2,22 +2,17 @@ package firstPackage.First.dailyExercises;
 
 public class ReverseLetters {
     public static void main(String[] args) {
-        String mockData = "Anu";
-        String[] arrayData = mockData.split(" ");
-        for (String temp : arrayData) {
-            System.out.println(temp);
-        }
-        for (int index = 0; index < 1; index++) {
-            char[] letter = arrayData[index].toCharArray();
-            for (int subIndex = letter.length - 1; subIndex >= 0; subIndex--) {
-                System.out.print(letter[subIndex]);
-            }
-            System.out.print(" ");
-        }
+        String input = "Anushree";
+        String reversed = reverseString(input);
+        System.out.println(reversed);
+    }
+
+    public static String reverseString(String input) {
+        return new StringBuilder(input).reverse().toString();
     }
 }
 //OUTPUT:
-// "C:\Program Files\Java\jdk-17\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2023.1\lib\idea_rt.jar=63969:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2023.1\bin" -Dfile.encoding=UTF-8 -classpath "C:\Users\anushrees\Desktop\New folder\DemoRepo\First\out\production\First1" firstPackage.First.dailyExercises.ReverseLetters
-//Anu
-//unA
+// "C:\Program Files\Java\jdk-17\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2023.1\lib\idea_rt.jar=52065:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2023.1\bin" -Dfile.encoding=UTF-8 -classpath "C:\Users\anushrees\Desktop\New folder\DemoRepo\First\out\production\First1" firstPackage.First.dailyExercises.ReverseLetters
+//eerhsunA
+//
 //Process finished with exit code 0
