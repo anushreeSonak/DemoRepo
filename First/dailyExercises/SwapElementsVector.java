7d420cabd6020bc3f8134fa7f4c98fab0a6ef3d2
Anushree Sonak <anushrees@synerzipune.local>
2023-05-24 16:54:36 +0530

    Added updated code of coding exercise-14

--- First/dailyExercises/SwapElementsVector.java
+++ First/dailyExercises/SwapElementsVector.java
@@ -5,22 +5,32 @@ import java.util.Vector;
 
 public class SwapElementsVector {
     public static void main(String[] args) {
-        Vector<String> data = new Vector();
-        data.add("1");
-        data.add("2");
-        data.add("3");
-        data.add("4");
-        data.add("5");
-        System.out.println(data);
-        Collections.swap(data, 0, 4);
-        System.out.println("Elements After swapping");
-        System.out.println(data);
+        // Create a Vector and add elements
+        Vector<String> data = new Vector<>();
+        data.add("A");
+        data.add("B");
+        data.add("C");
+        data.add("D");
+        data.add("E");
+        // Print the initial elements
+        System.out.println("Initial Vector: " + data);
+        swapElements(data, 0, 4);
+        // Print the Vector after swapping
+        System.out.println("Vector After Swapping: " + data);
+    }
+
+    public static void swapElements(Vector<String> vector, int index, int subIndex) {
+        if (index < 0 || index >= vector.size() || subIndex < 0 || subIndex >= vector.size()) {
+            System.out.println("Invalid indices. Unable to swap elements.");
+            return;
+        }
+        // Swap the elements using Collections.swap()
+        Collections.swap(vector, index, subIndex);
     }
 }
 //OUTPUT:
-// "C:\Program Files\Java\jdk-17\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2023.1\lib\idea_rt.jar=64662:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2023.1\bin" -Dfile.encoding=UTF-8 -classpath "C:\Users\anushrees\Desktop\New folder\DemoRepo\First\out\production\First1" firstPackage.First.dailyExercises.SwapElementsVector
-//[1, 2, 3, 4, 5]
-//Elements After swapping
-//[5, 2, 3, 4, 1]
+// "C:\Program Files\Java\jdk-17\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2023.1\lib\idea_rt.jar=57973:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2023.1\bin" -Dfile.encoding=UTF-8 -classpath "C:\Users\anushrees\Desktop\New folder\DemoRepo\First\out\production\First1" firstPackage.First.dailyExercises.SwapElementsVector
+//Initial Vector: [A, B, C, D, E]
+//Vector After Swapping: [E, B, C, D, A]
 //
 //Process finished with exit code 0
