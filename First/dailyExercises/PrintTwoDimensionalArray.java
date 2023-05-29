39e1173010145129f696ebec61404d791a71bc0e
Anushree Sonak <anushrees@synerzipune.local>
2023-05-29 13:01:18 +0530

    Added code of daily coding exercise-16

--- First/dailyExercises/PrintTwoDimensionalArray.java
+++ First/dailyExercises/PrintTwoDimensionalArray.java
@@ -13,9 +13,9 @@ public class PrintTwoDimensionalArray {
 
     public static void main(String args[])
             throws IOException {
-        int matrix[][] = {{1, 2, 3, 4},
+        int mat[][] = {{1, 2, 3, 4},
                 {5, 6, 7, 8},
                 {9, 10, 11, 12}};
-        print2D(matrix);
+        print2D(mat);
     }
 }
