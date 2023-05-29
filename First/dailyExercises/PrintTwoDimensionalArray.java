01608a20d3c0d9df06f0a5f9fb9c7d3e46de2337
Anushree Sonak <anushrees@synerzipune.local>
2023-05-29 12:23:33 +0530

    Added code of daily coding exercise-16

---
+++ First/dailyExercises/PrintTwoDimensionalArray.java
@@ -0,0 +1,21 @@
+package firstPackage.First.dailyExercises;
+
+import java.io.IOException;
+
+public class PrintTwoDimensionalArray {
+    public static void print2D(int mat[][]) {
+        // Loop through all rows
+        for (int index = 0; index < mat.length; index++)
+            // Loop through all elements of current row
+            for (int subIndex = 0; subIndex < mat[index].length; subIndex++)
+                System.out.print(mat[index][subIndex] + " ");
+    }
+
+    public static void main(String args[])
+            throws IOException {
+        int matrix[][] = {{1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12}};
+        print2D(matrix);
+    }
+}
