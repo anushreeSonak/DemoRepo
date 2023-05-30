5c8c20262786272e0d30c3ed78c710197a2e5098
Anushree Sonak <anushrees@synerzipune.local>
2023-05-30 14:15:57 +0530

    Added updated code of coding exercise-16

--- First/dailyExercises/PrintTwoDimensionalArray.java
+++ First/dailyExercises/PrintTwoDimensionalArray.java
@@ -3,19 +3,19 @@ package firstPackage.First.dailyExercises;
 import java.io.IOException;
 
 public class PrintTwoDimensionalArray {
-    public static void print2D(int mat[][]) {
+    public static void print2D(int mockData[][]) {
         // Loop through all rows
-        for (int index = 0; index < mat.length; index++)
+        for (int index = 0; index < mockData.length; index++)
             // Loop through all elements of current row
-            for (int subIndex = 0; subIndex < mat[index].length; subIndex++)
-                System.out.print(mat[index][subIndex] + " ");
+            for (int subIndex = 0; subIndex < mockData[index].length; subIndex++)
+                System.out.print(mockData[index][subIndex] + " ");
     }
 
     public static void main(String args[])
             throws IOException {
-        int mat[][] = {{1, 2, 3, 4},
+        int mockData[][] = {{1, 2, 3, 4},
                 {5, 6, 7, 8},
                 {9, 10, 11, 12}};
-        print2D(mat);
+        print2D(mockData);
     }
 }
