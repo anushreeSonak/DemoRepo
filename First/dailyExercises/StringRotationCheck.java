c8710436f41d7744a09952bd8ec4f48151fa5cb1
Anushree Sonak <anushrees@synerzipune.local>
2023-06-12 12:42:48 +0530

    Added READme File and Updated code

--- First/dailyExercises/StringRotationCheck.java
+++ First/dailyExercises/StringRotationCheck.java
@@ -1,6 +1,7 @@
 package firstPackage.First.dailyExercises;
 
 public class StringRotationCheck {
+
     public static boolean areRotations(String data, String mockData) {
         // Check if both strings have the same length
         if (data.length() != mockData.length()) {
@@ -22,7 +23,3 @@ public class StringRotationCheck {
         }
     }
 }
-//OUTPUT: "C:\Program Files\Java\jdk-17\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2023.1\lib\idea_rt.jar=55292:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2023.1\bin" -Dfile.encoding=UTF-8 -classpath "C:\Users\anushrees\Desktop\New folder\DemoRepo\First\out\production\First1" firstPackage.First.dailyExercises.StringRotationCheck
-//The strings are rotations of each other.
-//
-//Process finished with exit code 0
