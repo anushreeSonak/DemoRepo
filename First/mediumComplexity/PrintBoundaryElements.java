abd045568224b2c0edcb3cdc3fa0ab638848d742
Anushree Sonak <anushrees@synerzipune.local>
2023-06-13 12:44:46 +0530

    Added Updated code

--- First/mediumComplexity/PrintBoundaryElements.java
+++ First/mediumComplexity/PrintBoundaryElements.java
@@ -1,41 +1,35 @@
 package firstPackage.First.mediumComplexity;
 
 public class PrintBoundaryElements {
-    public void Boundary_Elements(int mat[][]){
-    // Printing Input Matrix;
+    public void boundaryElement(int data[][]) {
+        // Printing Input Matrix;
         System.out.println("Input Matrix is : ");
-        for(int index = 0; index < mat.length; index++) {
-        for (int subIndex = 0; subIndex < mat[index].length; subIndex++) {
-            System.out.print(mat[index][subIndex]);
+        for (int index = 0; index < data.length; index++) {
+            for (int subIndex = 0; subIndex < data[index].length; subIndex++) {
+                System.out.print(data[index][subIndex]);
+            }
+            System.out.println();
         }
-        System.out.println();
-    }
-    // Printing Boundary Values of Matrix
+        // Printing Boundary Values of Matrix
         System.out.println("Resultant Matrix is :");
-
-        for (int index = 0; index < mat.length; index++) {
-        for (int subIndex = 0; subIndex < mat[index].length; subIndex++) {
-
-            if (index == 0 || subIndex == 0 || index == mat.length - 1
-                    || subIndex == mat[index].length - 1) {
-                System.out.print(mat[index][subIndex]);
+        for (int index = 0; index < data.length; index++) {
+            for (int subIndex = 0; subIndex < data[index].length; subIndex++) {
+                if (index == 0 || subIndex == 0 || index == data.length - 1
+                        || subIndex == data[index].length - 1) {
+                    System.out.print(data[index][subIndex]);
+                } else {
+                    // Printing Space if element is not at Boundary
+                    System.out.print(" ");
+                }
             }
-            else {
-                // Printing Space if element
-                // is not at Boundary
-                System.out.print(" ");
-            }
+            System.out.println();
         }
-        System.out.println();
     }
-}
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         // Input Matrix
-        int mat[][] = new int[][] { { 1, 2, 3 },
-                { 4, 5, 6 },
-                { 7, 8, 9 } };
-        PrintBoundaryElements B_Values = new PrintBoundaryElements();
-        B_Values.Boundary_Elements(mat);
+        int data[][] = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        PrintBoundaryElements mockData = new PrintBoundaryElements();
+        mockData.boundaryElement(data);
     }
 }
