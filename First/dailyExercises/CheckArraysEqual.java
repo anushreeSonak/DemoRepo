fdc3e5ba64bb9dbccd1c34e009e37baa45c205ef
Anushree Sonak <anushrees@synerzipune.local>
2023-06-27 13:51:49 +0530

    Added Updated code

--- First/dailyExercises/CheckArraysEqual.java
+++ First/dailyExercises/CheckArraysEqual.java
@@ -1,6 +1,7 @@
 package firstPackage.First.dailyExercises;
 
-public class checkArraysEqual {
+public class CheckArraysEqual {
+
     public static void main(String[] args) {
         // Initializing the first array
         int array[] = {10, 30, 12};
@@ -30,8 +31,3 @@ public class checkArraysEqual {
         }
     }
 }
-//OUTPUT:
-// "C:\Program Files\Java\jdk-17\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2023.1\lib\idea_rt.jar=51073:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2023.1\bin" -Dfile.encoding=UTF-8 -classpath "C:\Users\anushrees\Desktop\New folder\DemoRepo\First\out\production\First1" firstPackage.First.dailyExercises.checkArraysEqual
-//Arrays are not equal
-//
-//Process finished with exit code 0
